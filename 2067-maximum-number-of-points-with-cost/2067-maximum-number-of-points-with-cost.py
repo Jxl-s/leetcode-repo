b5bf597352fe5e729fea188e3512501c691d7cbd
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-08-20 10:58:20 -0400

    Time: 1525 ms (55.04%), Space: 50.2 MB (69.52%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
@@ -2,16 +2,16 @@ class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         dp = points[0]
         for i in range(1, len(points)):
-            row_left = dp[:]
-            row_right = dp[:]
+            left = dp[:]
+            right = dp[:]
 
-            for j in range(1, len(row_left)):
-                row_left[j] = max(dp[j], row_left[j - 1] - 1)
-            
-            for j in range(len(row_left) - 2, -1, -1):
-                row_right[j] = max(dp[j], row_right[j + 1] - 1)
-            
-            for j in range(len(row_left)):
-                dp[j] = points[i][j] + max(row_left[j], row_right[j])
+            for j in range(1, len(points[i])):
+                left[j] = max(dp[j], left[j - 1] - 1)
 
+            for j in range(len(points[i]) - 2, -1, -1):
+                right[j] = max(dp[j], right[j + 1] - 1)
+
+            for j in range(len(dp)):
+                dp[j] = points[i][j] + max(left[j], right[j])
+        
         return max(dp)
