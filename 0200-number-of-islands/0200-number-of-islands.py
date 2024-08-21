887d95a77935597eb4595ce3a378c6510d0c389a
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-08-21 08:53:32 -0400

    Time: 240 ms (63.55%), Space: 18.9 MB (79.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        def flood(i, j):
-            if i < 0 or i >= len(grid):
-                return
-            
-            if j < 0 or j >= len(grid[i]):
+        visited = [[False] * len(m) for m in grid]
+        count = 0
+
+        def dfs(i, j):
+            if i < 0 or j < 0:
                 return
 
-            if (i, j) in visited:
+            if i >= len(grid) or j >= len(grid[0]):
+                return
+
+            if visited[i][j]:
                 return
             
-            visited.add((i, j))
+            visited[i][j] = True
             if grid[i][j] == "1":
-                grid[i][j] = "0"
-                flood(i + 1, j)
-                flood(i - 1, j)
-                flood(i, j + 1)
-                flood(i, j - 1)
-
-        island_count = 0
+                dfs(i + 1, j)
+                dfs(i - 1, j)
+                dfs(i, j + 1)
+                dfs(i, j - 1)
+        
+        count = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if grid[i][j] == "1":
-                    flood(i, j)
-                    island_count += 1
+                if grid[i][j] == "1" and not visited[i][j]:
+                    count += 1
+                    dfs(i, j)
 
-        return island_count
+        return count
