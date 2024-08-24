b4234e57ac412306e4d25cd942b704bd71c8298d
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-08-24 01:27:08 -0400

    Time: 30 ms (89.61%), Space: 16.6 MB (28.21%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
@@ -1,38 +1,37 @@
 class Solution:
     def nearestPalindromic(self, n: str) -> str:
-        # one digit numbers
-        if len(n) <= 1:
+        # case: < 10
+        if len(n) == 1:
             return str(int(n) - 1)
         
-        # all 9's
-        if all(c == "9" for c in n):
+        # case: 100 ... 000
+        if n[0] == '1' and all(c == '0' for c in n[1:]):
+            return str(int(n) - 1)
+
+        # case: 100 ... 001
+        if n[0] == '1' and n[-1] == '1' and all(c == '0' for c in n[1:-1]):
+            return str(int(n) - 2)
+        
+        # case: 999 ... 999
+        if all(c == '9' for c in n):
             return str(int(n) + 2)
 
-        # 100 ... 001
-        if n[0] == "1" and n[-1] == "1" and all(c == "0" for c in n[1:-1]):
-            return str(int(n) - 2)
-
-        # 100000
-        if n[0] == "1" and all(c == "0" for c in n[1:]):
-            return str(int(n) - 1)
+        half_str = n[:(len(n)+1)//2]
+        lower_str = str(int(half_str) - 1)
+        higher_str = str(int(half_str) + 1)
 
         possible = set()
 
-        half = n[: (len(n) + 1) // 2]
-        lowered = str(int(half) - 1)
-        highered = str(int(half) + 1)
-
-        # flipping normally, lowering, highering
         if len(n) % 2 == 0:
-            possible.add(half + half[::-1])
-            possible.add(lowered + lowered[::-1])
-            possible.add(highered + highered[::-1])
+            possible.add(half_str + half_str[::-1])
+            possible.add(lower_str + lower_str[::-1])
+            possible.add(higher_str + higher_str[::-1])
         else:
-            possible.add(half + half[::-1][1:])
-            possible.add(lowered + lowered[::-1][1:])
-            possible.add(highered + highered[::-1][1:])
-
+            possible.add(half_str + half_str[::-1][1:])
+            possible.add(lower_str + lower_str[::-1][1:])
+            possible.add(higher_str + higher_str[::-1][1:])
+        
         if n in possible:
             possible.remove(n)
 
-        return min(possible, key=lambda x: (abs(int(x) - int(n)), int(x)))
+        return min(possible, key=lambda x: (abs(int(n) - int(x)), int(x)))
