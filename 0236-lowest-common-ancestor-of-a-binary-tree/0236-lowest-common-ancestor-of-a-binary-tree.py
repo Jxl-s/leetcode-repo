17c5660300bc8a76110c65a6cfccef560ffcc71c
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-08-25 16:33:45 -0400

    Time: 51 ms (52.35%), Space: 21.1 MB (23.43%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,37 +7,16 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path_p = None
-        path_q = None
-
-        def dfs(node, path):
-            nonlocal path_p, path_q
-            if not node:
-                return
-            
-            if node == p:
-                path_p = path[:]
-                path_p.append(node)
-
-            if node == q:
-                path_q = path[:]
-                path_q.append(node)
-
-            if path_p and path_q:
-                return
-
-            copy = path[:]
-            copy.append(node)
-
-            dfs(node.left, copy)
-            dfs(node.right, copy)
-
-        dfs(root, [])
-        last_good = path_q[0]
-        for i in range(min(len(path_p), len(path_q))):
-            if path_p[i] == path_q[i]:
-                last_good = path_p[i]
-            else:
-                break
+        if not root:
+            return None
         
-        return last_good
+        if root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
