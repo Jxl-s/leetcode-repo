02ea8d55f56c1e9fd39f62d76a8368bbc74d96fb
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-08-27 22:44:08 -0400

    Time: 104 ms (79.76%), Space: 16.9 MB (86.35%) - LeetHub

--- 0879-maximize-distance-to-closest-person/0879-maximize-distance-to-closest-person.py
+++ 0879-maximize-distance-to-closest-person/0879-maximize-distance-to-closest-person.py
@@ -1,36 +1,19 @@
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
-        n = len(seats)
-
-        left = [0 for _ in range(n)]
-        right = [0 for _ in range(n)]
-        
-        left_seat = -1
-
-        for i in range(n):
-            if seats[i] == 1:
-                left_seat = i
-
-            if left_seat == -1:
-                left[i] = -1
-            else:
-                left[i] = i - left_seat
-
-        right_seat = -1
-        for i in range(n - 1, -1, -1):
-            if seats[i] == 1:
-                right_seat = i
-
-            if right_seat == -1:
-                right[i] = -1
-            else:
-                right[i] = right_seat - i
-
         max_dist = 0
+        last_seat = -1
+
+        n = len(seats)
         for i in range(n):
-            if right[i] == -1 or left[i] == -1:
-                max_dist = max(max_dist, max(left[i], right[i]))
-            elif left[i] > 0:
-                max_dist = max(max_dist, min(left[i], right[i]))
+            if seats[i] == 1:
+                if last_seat == -1:
+                    max_dist = i
+                else:
+                    max_dist = max(max_dist, (i - last_seat) // 2)
+
+                last_seat = i
+
+        if seats[-1] != 1:
+            max_dist = max(max_dist, n - 1 - last_seat)
 
         return max_dist
