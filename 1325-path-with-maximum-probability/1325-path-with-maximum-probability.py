94e9dc5590ee983e5d07260de784fe4d460a832e
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-08-30 23:37:03 -0400

    Time: 540 ms (75.4%), Space: 28.5 MB (50.13%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -1,28 +1,29 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         adj = [[] for i in range(n)]
+
         for i in range(len(edges)):
             a, b = edges[i]
-            prob = succProb[i]
+            w = succProb[i]
 
-            adj[a].append((b, prob))
-            adj[b].append((a, prob))
+            adj[a].append((b, w))
+            adj[b].append((a, w))
 
+        visited = {}
         heap = [(-1, start_node)]
-        max_prob = [0] * n
-        max_prob[start_node] = 1
 
         while len(heap) > 0:
-            cur_prob, cur_node = heappop(heap)
-            cur_prob = -cur_prob
+            w, node = heappop(heap)
+            w = -w
+            if node in visited:
+                continue
 
-            if cur_node == end_node:
-                return cur_prob
+            visited[node] = w
+            if node == end_node:
+                return w
 
-            for neighbor_node, neighbor_prob in adj[cur_node]:
-                new_prob = cur_prob * neighbor_prob
-                if new_prob > max_prob[neighbor_node]:
-                    max_prob[neighbor_node] = new_prob
-                    heappush(heap, (-new_prob, neighbor_node))
+            for n2, w2 in adj[node]:
+                if n2 not in visited:
+                    heappush(heap, (-w * w2, n2))
 
         return 0
