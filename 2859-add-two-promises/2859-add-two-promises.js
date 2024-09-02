90a207f4124b930d3fbb0d1b997e47465e6edce8
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-09-01 22:54:04 -0400

    Time: 68 ms (29.64%), Space: 49.7 MB (40.68%) - LeetHub

---
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [r1, r2] = await Promise.all([promise1, promise2]);
+    return r1 + r2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
