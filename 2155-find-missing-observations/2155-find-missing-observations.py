7f69a877d0c49e54c714fa30daf9d31b05ca6ef5
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-09-04 20:11:18 -0400

    Time: 985 ms (62.03%), Space: 27.1 MB (49.47%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.py
+++ 2155-find-missing-observations/2155-find-missing-observations.py
@@ -7,13 +7,12 @@ class Solution:
         if avg_roll > 6 or avg_roll < 1:
             return []
 
-        if remainder == 0:        
-            return [avg_roll] * n
-
         result = [avg_roll] * n
-
-        # distribute the extra
         max_give = 6 - avg_roll
+
+        if remainder > max_give * n:
+            return []
+
         for i in range(len(result)):
             if remainder <= 0:
                 break
@@ -21,8 +20,5 @@ class Solution:
             give_count = min(max_give, remainder)
             remainder -= give_count
             result[i] += give_count
-
-        if remainder > 0:
-            return []
 
         return result
