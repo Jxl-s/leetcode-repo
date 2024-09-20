19ba72e5057c14137b5dca781e0d378d5374d622
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-09-19 23:39:14 -0400

    Time: 665 ms (20.43%), Space: 17 MB (41.65%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,13 +1,13 @@
 class Solution:
-    def isPalindrome(self, s):
-        return s[::-1] == s
-
     def shortestPalindrome(self, s: str) -> str:
         if not s:
             return s
 
+        def isPalindrome(s):
+            return s[::-1] == s
+
         s2 = s[::-1]
         for i in range(len(s2)):
-            if self.isPalindrome(s2[:i] + s):
+            if isPalindrome(s2[:i] + s):
                 return s2[:i] + s
         
