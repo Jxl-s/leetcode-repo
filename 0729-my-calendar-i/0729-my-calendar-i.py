101cebcb7e3b1349db07d6718492eb9d3edb2525
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-09-25 23:57:24 -0400

    Time: 181 ms (65.37%), Space: 17.4 MB (65.25%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,9 +1,8 @@
 class TreeNode:
-    def __init__(self,val=None):
-        self.value = val
-
-        self.left = None
-        self.right = None
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left, self.right = None, None
 
 class MyCalendar:
     def __init__(self):
@@ -15,13 +14,13 @@ class MyCalendar:
 
     def _insert(self, parent, start, end):
         if not parent:
-            return (TreeNode((start, end)), True)
+            return (TreeNode(start, end), True)
 
-        if start < parent.value[1] and end > parent.value[0]:
+        if start < parent.end and end > parent.start:
             return (parent, False)
 
         can_book = True
-        if start >= parent.value[1]:
+        if start >= parent.end:
             parent.right, can_book = self._insert(parent.right, start, end)
         else:
             parent.left, can_book = self._insert(parent.left, start, end)
