35bd4a552492b8ff083dd9702e5f012b75a6af92
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-16 21:19:43 -0400

    Time: 35 ms (51.7%), Space: 16.6 MB (10.8%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,12 +1,10 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        def swap(i, j):
-            as_str = str(num)
-            return int(as_str[:i] + as_str[j] + as_str[i+1:j] + as_str[i] + as_str[j+1:])
-
+        as_str = str(num)
         max_num = num
-        for i in range(len(str(num))):
-            for j in range(i + 1, len(str(num))):
-                max_num = max(max_num, swap(i, j))
+
+        for i in range(len(as_str)):
+            for j in range(i + 1, len(as_str)):
+                max_num = max(max_num, int(as_str[:i] + as_str[j] + as_str[i+1:j] + as_str[i] + as_str[j+1:]))
 
         return max_num
