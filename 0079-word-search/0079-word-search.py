86cdd9150ce2123e572ff558e99c0db6d29aa0f3
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-17 21:54:42 -0400

    Time: 3602 ms (74.85%), Space: 16.9 MB (9.35%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,41 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        stack = [c for c in word]
-        m = len(board)
-        n = len(board[0])
+        m, n = len(board), len(board[0])
+        visited = [[False] * n for _ in range(m)]
 
-        visited = set()
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        def dfs(remaining, i, j):
-            if i < 0 or i >= m:
-                return False
-            
-            if j < 0 or j >= n:
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+        def backtrack(i, j, index):
+            if i < 0 or i >= m or j < 0 or j >= n:
                 return False
 
-            if (i, j) in visited:
+            if visited[i][j]:
                 return False
 
-            if board[i][j] != remaining[0]:
-                return False
-
-            copy = remaining[1:]
-            if len(copy) == 0:
-                return True
-
-            visited.add((i, j))
-
-            for x, y in directions:
-                if dfs(copy, i + y, j + x):
-                    visited.remove((i, j))
+            visited[i][j] = True
+            if board[i][j] == word[index]:
+                if index + 1 == len(word):
                     return True
 
-            visited.remove((i, j))
+                for di, dj in directions:
+                    if backtrack(i + di, j + dj, index + 1):
+                        return True
+
+            visited[i][j] = False
             return False
-        
+
         for i in range(m):
             for j in range(n):
-                if dfs(stack, i, j):
+                if board[i][j] == word[0] and backtrack(i, j, 0):
                     return True
 
         return False
