bc70508c0d233eae0271806f8dbf60da70f3f2f9
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-17 22:40:36 -0400

    Time: 4560 ms (61.21%), Space: 17.9 MB (90.75%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -13,29 +13,32 @@ class Solution:
 
             root['.'] = w
 
-        output = set()
+        output = []
         visited = [[False] * n for i in range(m)]
         directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
         def dfs(i, j, root):
+            item = board[i][j]
             if visited[i][j]:
                 return
 
             visited[i][j] = True
+            if '.' in root[item]:
+                word = root[item]['.']
 
-            if '.' in root[board[i][j]]:
-                output.add(root[board[i][j]]['.'])
+                output.append(word)
+                del root[item]['.']
 
             for di, dj in directions:
                 ni, nj = i + di, j + dj
-                if ni >= 0 and ni < m and nj >= 0 and nj < n and board[ni][nj] in root[board[i][j]]:
-                    dfs(ni, nj, root[board[i][j]])
+                if ni >= 0 and ni < m and nj >= 0 and nj < n and board[ni][nj] in root[item]:
+                    dfs(ni, nj, root[item])
 
             visited[i][j] = False
-        
+
         for i in range(m):
             for j in range(n):
                 if board[i][j] in trie:
                     dfs(i, j, trie)
 
-        return list(output)
+        return output
