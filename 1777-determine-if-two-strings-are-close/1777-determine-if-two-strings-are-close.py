b7529a3b88616b301964e870829c9dc56100db9f
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-19 16:31:11 -0400

    Time: 75 ms (97.16%), Space: 17.1 MB (99.59%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -1,8 +1,10 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        # Operation 1: Swapping characters (must use same chars)
         if set(word1) != set(word2):
             return False
 
+        # Operation 2: Transforming (counts must be similar)
         counter_1 = Counter(word1)
         counter_2 = Counter(word2)
 
