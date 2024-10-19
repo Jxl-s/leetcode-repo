36b16900ecfa7b1d2d03a8b2c2a21657a5083bf6
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-18 22:59:46 -0400

    Time: 3 ms (100%), Space: 16.7 MB (6.69%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,38 +1,35 @@
-from collections import deque
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        orange_count = 0
+        m, n = len(grid), len(grid[0])
+
+        oranges = 0
         queue = deque()
 
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 1:
-                    orange_count += 1
-
+                    oranges += 1
+                
                 if grid[i][j] == 2:
                     queue.append((i, j, 0))
 
-        if orange_count == 0:
+        if oranges == 0:
             return 0
 
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         while len(queue) > 0:
-            i, j, time = queue.popleft()
-
-            # explore neighbors
-            for dy, dx in directions:
-                ny, nx = i + dy, j + dx
-                # check bounds
-                if ny >= 0 and ny < len(grid) and nx >= 0 and nx < len(grid[0]):
-                    # rot the orange
-                    if grid[ny][nx] == 1:
-                        grid[ny][nx] = 2
-                        orange_count -= 1
-
-                        if orange_count == 0:
-                            return time + 1
-
-                        queue.append((ny, nx, time + 1))
+            i, j, minutes = queue.popleft()
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if ni < 0 or ni >= m or nj < 0 or nj >= n:
+                    continue
+                
+                if grid[ni][nj] == 1:
+                    oranges -= 1
+                    if oranges == 0:
+                        return minutes + 1
+                    
+                    grid[ni][nj] = 2
+                    queue.append((ni, nj, minutes + 1))
 
         return -1
