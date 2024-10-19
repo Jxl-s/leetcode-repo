cf3edd5442bdbd17aa39ff91125f2a55b2857d8b
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-19 16:45:45 -0400

    Time: 27 ms (99.77%), Space: 21 MB (99.11%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -1,23 +1,21 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        row_hash = {}
         n = len(grid)
 
+        rows = defaultdict(int)
+        columns = defaultdict(int)
+
         for i in range(n):
-            t = tuple(grid[i])
-            if t in row_hash:
-                row_hash[t] += 1
-            else:
-                row_hash[t] = 1
+            rows[tuple(grid[i])] += 1
+
+            col = [0] * n
+            for j in range(n):
+                col[j] = grid[j][i]
+
+            columns[tuple(col)] += 1
         
         pairs = 0
-        for j in range(n):
-            col = []
-            for i in range(n):
-                col.append(grid[i][j])
-
-            t = tuple(col)
-            if t in row_hash:
-                pairs += row_hash[t]
+        for hashed in rows.keys():
+            pairs += rows[hashed] * columns[hashed]
 
         return pairs
