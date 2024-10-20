0b355287ee9ac71d1513353fd04e9093bb60c60d
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-19 20:43:37 -0400

    Time: 7 ms (100%), Space: 16.8 MB (19.76%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.py
@@ -0,0 +1,31 @@
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        operators = '!&|'
+
+        stack = []
+        current = ''
+
+        for c in expression:
+            if c in operators:
+                symbol = c
+            elif c == '(':
+                stack.append((symbol, current))
+                symbol, current = '', ''
+            elif c == ')':
+                prev_symbol, prev_expression = stack.pop()
+                result = 't'
+
+                if prev_symbol == '&' and 'f' in current:
+                    result = 'f'
+                
+                if prev_symbol == '|' and 't' not in current:
+                    result = 'f'
+                
+                if prev_symbol == '!' and 't' in current:
+                    result = 'f'
+
+                current = prev_expression + result
+            else:
+                current += c
+
+        return current == 't'
