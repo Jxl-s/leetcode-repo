743de88c98209cc2a0d21e5028a58f19f17b86d9
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-10-19 21:15:09 -0400

    Time: 19 ms (99.83%), Space: 19.8 MB (99.36%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,46 +1,41 @@
-from typing import List
+class UnionFind:
+    def __init__(self, n):
+        self.parents = list(range(n))
 
+    def find(self, key):
+        if self.parents[key] != key:
+            self.parents[key] = self.find(self.parents[key])
+
+        return self.parents[key]
+
+    def union(self, a, b):
+        self.parents[self.find(a)] = self.find(b)
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        all_accounts = {}
-        for acc in accounts:
-            if acc[0] in all_accounts:
-                all_accounts[acc[0]].append(acc[1:])
-            else:
-                all_accounts[acc[0]] = [acc[1:]]
+        union_find = UnionFind(len(accounts))
+        owners = {} # email -> owner
+
+        for i in range(len(accounts)):
+            owner = accounts[i][0]
+            # go through aech email
+            for j in range(1, len(accounts[i])):
+                email = accounts[i][j]
+                # if email is already owned, link the owner
+                if email in owners:
+                    union_find.union(i, owners[email])
+
+                owners[email] = i
+        
+        email_list = {}
+        for email, owner in owners.items():
+            key = union_find.find(owner)
+            if key not in email_list: email_list[key] = []
+
+            email_list[key].append(email)
         
         output = []
-        for name, accs in all_accounts.items():
-            adj = {}
-            for i, email_list in enumerate(accs):
-                for email in email_list:
-                    if email in adj:
-                        adj[email].append(i)
-                    else:
-                        adj[email] = [i]
-            
-            # Start traversal
-            visited = set()
-            def dfs(email):
-                if email in visited:
-                    return []
-
-                visited.add(email)
-                all_emails = [email]
-                for i in adj[email]:
-                    for email in accs[i]:
-                        all_emails.extend(dfs(email))
-
-                return all_emails
-            
-            email_groups = []
-            for email in adj.keys():
-                result = dfs(email)
-                if len(result) > 0:
-                    email_groups.append(result)
-            
-            for group in email_groups:
-                output.append([name, *sorted(group)])
+        for owner, emails in email_list.items():
+            output.append([accounts[owner][0], *sorted(emails)])
 
         return output
