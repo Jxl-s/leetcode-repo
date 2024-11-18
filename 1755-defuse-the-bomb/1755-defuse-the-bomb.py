bf121ccd2106412ab40dbe60144a446ed5ea295b
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-11-17 21:31:25 -0500

    Time: 3 ms (56.07%), Space: 16.9 MB (5.59%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        prefix = [0] * (2 * n + 1)
+
+        for i in range(2 * n):
+            prefix[i + 1] = prefix[i] + code[i % n]
+
+        output = [0] * n
+        if k == 0:
+            return output
+
+        if k > 0:
+            for i in range(n):
+                output[i] = prefix[i + k + 1] - prefix[i + 1]
+            
+            return output
+        
+        if k < 0:
+            for i in range(n):
+                output[i] = prefix[i + n] - prefix[i + n + k]
+
+            return output
