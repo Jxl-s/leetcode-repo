367b9f1b56bc5a2373b21c8c11e1d9bf7fc098df
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-11-20 21:27:12 -0500

    Time: 268 ms (96%), Space: 38.2 MB (77.78%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
@@ -1,40 +1,27 @@
-UNGUARDED = 0
-OBSTACLE = 1
-GUARDED = 2
-
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        matrix = [[UNGUARDED] * n for _ in range(m)]
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        matrix = [[0] * n for _ in range(m)]
+
         for i, j in guards:
-            matrix[i][j] = OBSTACLE
+            matrix[i][j] = 1
 
         for i, j in walls:
-            matrix[i][j] = OBSTACLE
-
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            matrix[i][j] = 1
+        
+        blocked = 0
         for i, j in guards:
-            # Go all the way in all directions
             for di, dj in directions:
                 ni, nj = i + di, j + dj
+                while 0 <= ni < m and 0 <= nj < n:
+                    if matrix[ni][nj] == 1:
+                        break
 
-                while True:
-                    # Boundaries
-                    if ni < 0 or nj < 0: break
-                    if ni >= m or nj >= n: break
-
-                    # Obstacle
-                    if matrix[ni][nj] == OBSTACLE: break
-
-                    if matrix[ni][nj] == UNGUARDED:
-                        matrix[ni][nj] = GUARDED
+                    if matrix[ni][nj] == 0:
+                        matrix[ni][nj] = 2
+                        blocked += 1
 
                     ni += di
                     nj += dj
-        
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == UNGUARDED:
-                    count += 1
 
-        return count
+        return m * n - len(guards) - len(walls) - blocked
