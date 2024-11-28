56eea64c46aec8af879f71b4239cd64df807afee
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-11-27 23:30:57 -0500

    Time: 40 ms (17.01%), Space: 17 MB (8.49%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -8,13 +8,13 @@ class Solution:
             bit_b = (b >> i) & 1
             bit_c = (c >> i) & 1
 
+            # Case 0: make both 0
             if bit_c == 0:
                 if bit_a == 1: count += 1
                 if bit_b == 1: count += 1
-            
-            if bit_c == 1:
-                # make only one of them 1
-                if bit_a == 0 and bit_b == 0:
-                    count += 1
+
+            # Case 1: make at least one 1
+            if bit_c == 1 and bit_a == 0 and bit_b == 0:
+                count += 1
 
         return count
