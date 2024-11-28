ffd2bfef1a28efe296f937b70a176005858e6c31
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-11-27 21:49:25 -0500

    Time: 856 ms (92.18%), Space: 42.8 MB (60.67%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.py
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.py
@@ -3,25 +3,25 @@ class Solution:
         m, n = len(grid), len(grid[0])
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        obstacles = [[float('inf')] * n for _ in range(m)] 
-        obstacles[0][0] = 0
+        distances = [[float('inf')] * n for _ in range(m)]
+        distances[0][0] = 0
 
-        heap = [(0, 0, 0)] # removal, i, j
-        while heap:
-            removal, i, j = heapq.heappop(heap)
-            if removal > obstacles[i][j]:
-                continue
-            
+        queue = deque([(0, 0)])
+        while len(queue) > 0:
+            i, j = queue.popleft()
             for di, dj in directions:
                 ni, nj = i + di, j + dj
-                if ni < 0 or nj < 0 or ni >= m or nj >= n:
-                    continue
-
-                new_removal = removal + grid[ni][nj]
-                if new_removal >= obstacles[ni][nj]:
+                if ni < 0 or ni >= m or nj < 0 or nj >= n:
                     continue
                 
-                obstacles[ni][nj] = new_removal
-                heapq.heappush(heap, (new_removal, ni, nj))
-        
-        return obstacles[m - 1][n - 1]
+                new_distance = distances[i][j] + grid[ni][nj]
+                if new_distance >= distances[ni][nj]:
+                    continue
+                
+                distances[ni][nj] = new_distance
+                if grid[ni][nj] == 0:
+                    queue.appendleft((ni, nj))
+                else:
+                    queue.append((ni, nj))
+
+        return distances[m - 1][n - 1]
