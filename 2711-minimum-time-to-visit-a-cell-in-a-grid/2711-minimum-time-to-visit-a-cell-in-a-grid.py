6558f97361510caa59284f13ca66fe6aa5a006f6
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-11-28 23:50:42 -0500

    Time: 735 ms (77.88%), Space: 32.5 MB (62.41%) - LeetHub

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -6,29 +6,33 @@ class Solution:
         m, n = len(grid), len(grid[0])
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        visited = [[False] * n for _ in range(m)]
-        heap = [(0, 0, 0)]
+        distances = [[float('inf')] * n for _ in range(m)]
+        distances[0][0] = 0
 
+        heap = [(0, 0, 0)]
         while heap:
             time, i, j = heapq.heappop(heap)
+            if time > distances[i][j]:
+                continue
+
             for di, dj in directions:
                 ni, nj = i + di, j + dj
-                if ni < 0 or nj < 0 or ni >= m or nj >= n or visited[ni][nj]:
+                if ni < 0 or nj < 0 or ni >= m or nj >= n:
                     continue
 
                 delay = 1
-                # if can't do one step, move between 2 nodes until it reaches time
                 if time + delay < grid[ni][nj]:
-                    # that caused floating point error:
-                    # delay = ceil((grid[ni][nj] - time) / 2) * 2 + 1
                     delay = grid[ni][nj] - time
                     if delay % 2 == 0:
                         delay += 1
 
+                if time + delay >= distances[ni][nj]:
+                    continue
+
                 if ni == m - 1 and nj == n - 1:
                     return time + delay
 
-                visited[ni][nj] = True
+                distances[ni][nj] = time + delay
                 heapq.heappush(heap, (time + delay, ni, nj))
 
         return -1
