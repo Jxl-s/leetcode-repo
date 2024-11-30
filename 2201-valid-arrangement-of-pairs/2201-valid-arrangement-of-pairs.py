17be50138127450cd17e659d69beb7a79b6a68ab
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-11-29 21:18:04 -0500

    Time: 339 ms (58.37%), Space: 74.9 MB (94.14%) - LeetHub

---
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.py
@@ -0,0 +1,45 @@
+class Solution:
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        adj = defaultdict(list)
+        degree = defaultdict(int)
+
+        for a, b in pairs:
+            adj[a].append(b)
+            degree[a] += 1
+            degree[b] -= 1
+
+        def euler(node):
+            stack = [node]
+            circuit = []
+
+            while stack:
+                v = stack[-1]
+                if adj[v]:
+                    u = adj[v].pop()
+                    stack.append(u)
+                else:
+                    circuit.append(stack.pop())
+            
+            return circuit[::-1]
+
+        # Find start euler node
+        start = None
+        for node in adj.keys():
+            if degree[node] == 1:
+                start = node
+                break
+        
+        # Not found, use any with an edge
+        if start is None:
+            for node in adj.keys():
+                if len(adj[node]) > 0:
+                    start = node
+                    break
+
+        circuit = euler(start)
+        answer = []
+
+        for i in range(1, len(circuit)):
+            answer.append([circuit[i - 1], circuit[i]])
+        
+        return answer
