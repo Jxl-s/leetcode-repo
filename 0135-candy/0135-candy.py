fe0224526e5054be48222f8e92d3513ed191f016
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-12-20 13:09:40 -0500

    Time: 33 ms (9.14%), Space: 20.8 MB (5.43%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,36 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+
+        candies_left = [0] * n
+        candies_left[0] = 1
+
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies_left[i] = candies_left[i - 1] + 1
+            else:
+                candies_left[i] = 1
+
+        candies_right = [0] * n
+        candies_right[n-1] = 1
+
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies_right[i] = candies_right[i + 1] + 1
+            else:
+                candies_right[i] = 1
+        
+        candies = [0] * n
+        for i in range(n):
+            candies[i] = candies_left[i] + candies_right[i]
+        
+        low = min(candies) - 1
+        for i in range(n):
+            candies[i] -= low
+
+        for i in range(1, n - 1):
+            diff = min(candies[i] - candies[i - 1], candies[i] - candies[i + 1])
+            if diff > 1:
+                candies[i] -= (diff - 1)
+        
+        return sum(candies)
