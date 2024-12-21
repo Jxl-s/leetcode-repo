0e983ebef06fcc05373135921fdbd829e45af23f
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-12-21 01:30:03 -0500

    Time: 107 ms (90.24%), Space: 38.6 MB (24.39%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.py
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.py
@@ -9,21 +9,15 @@ class Solution:
         def dfs(node, parent):
             nonlocal count
 
-            # postorder
-            current_sum = 0
+            total = values[node]
             for neighbor in adj[node]:
-                if neighbor == parent:
-                    continue
+                if neighbor != parent:
+                    total += dfs(neighbor, node)
 
-                current_sum += dfs(neighbor, node)
-
-            current_sum += values[node]
-            current_sum %= k
-
-            if current_sum == 0:
+            if total % k == 0:
                 count += 1
 
-            return current_sum
-
+            return total
+        
         dfs(0, -1)
         return count
