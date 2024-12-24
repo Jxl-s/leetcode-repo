4dd0e52429f0a191119fa3c22b2142b59c89d3c0
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-12-24 00:14:51 -0500

    Time: 39 ms (93.5%), Space: 25.4 MB (75.61%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,18 +1,39 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        adj = { i: set() for i in range(n) }
+        if len(edges) == 0:
+            return [0]
+
+        adj = [[] for _ in range(n)]
+        degree = [0] * n
+
         for a, b in edges:
-            adj[a].add(b)
-            adj[b].add(a)
-        
-        while len(adj) > 2:
-            alone = []
-            for node, neighbors in adj.items():
-                if len(neighbors) == 1:
-                    alone.append((node, next(iter(neighbors))))
-            
-            for node, neighbor in alone:
-                del adj[node]
-                adj[neighbor].remove(node)
-        
-        return list(adj.keys())
+            adj[a].append(b)
+            adj[b].append(a)
+
+            degree[a] += 1
+            degree[b] += 1
+
+        queue = deque()
+        remaining = n
+
+        for i in range(n):
+            if degree[i] == 1:
+                queue.append(i)
+                remaining -= 1
+
+        while queue:
+            if remaining == 0:
+                return list(queue)
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                degree[node] = 0
+
+                for neighbor in adj[node]:
+                    if degree[neighbor] == 0:
+                        continue
+                    
+                    degree[neighbor] -= 1
+                    if degree[neighbor] == 1:
+                        queue.append(neighbor)
+                        remaining -= 1
