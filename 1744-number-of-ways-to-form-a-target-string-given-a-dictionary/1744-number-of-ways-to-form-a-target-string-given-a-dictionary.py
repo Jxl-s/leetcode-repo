0b86fb99604f5147a93c39a8f42bf5731ecb9387
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-12-28 23:17:39 -0500

    Time: 1535 ms (44.06%), Space: 261.7 MB (22.14%) - LeetHub

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,28 +1,25 @@
-MOD = 10**9 + 7
-
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        counts = [[0] * 26 for _ in range(len(words[0]))]
+        word_len = len(words[0])
+        target_len = len(target)
+
+        counts = [defaultdict(int) for _ in range(word_len)]
 
         for word in words:
-            for i, c in enumerate(word):
-                counts[i][ord(c) - ord('a')] += 1
+            for i in range(word_len):
+                counts[i][word[i]] += 1
 
-        # ith character of target, kth character of words
         @cache
-        def dp(k, i):
-            if i == len(target):
-                return 1
-            
-            if k == len(words[0]):
-                return 0
+        def dp(i, k):
+            if i == len(target): return 1
+            if k == len(words[0]): return 0
 
-            count = dp(k + 1, i)
-            possible = counts[k][ord(target[i]) - ord('a')]
+            count = dp(i, k + 1)
+            char_count = counts[k][target[i]]
 
-            if possible > 0:
-                count += possible * dp(k + 1, i + 1)
-                count %= MOD
+            if char_count > 0:
+                count += char_count * dp(i + 1, k + 1)
+                count %= (10**9 + 7)
 
             return count
 
