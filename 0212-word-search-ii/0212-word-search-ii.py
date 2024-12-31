0ab239e01f7723e5a120c933eabd3032ac671ae2
Jia <77560326+Jxl-s@users.noreply.github.com>
2024-12-31 12:25:18 -0500

    Time: 429 ms (94.22%), Space: 20.1 MB (9.85%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,44 +1,59 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        m, n = len(board), len(board[0])
-
         trie = {}
-        for w in words:
+
+        m, n = len(board), len(board[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        def trie_add(word):
             root = trie
-            for c in w:
+            for c in word:
                 if c not in root:
                     root[c] = {}
 
                 root = root[c]
 
-            root['.'] = w
+            root['.'] = word
+
+        def trie_del(node, word, depth=0):
+            if depth == len(word):
+                del node['.']
+                return None if len(node) == 0 else node
+
+            node[word[depth]] = trie_del(node[word[depth]], word, depth + 1)
+            if node[word[depth]] is None:
+                del node[word[depth]]
+
+            return None if len(node) == 0 else node
+
+        for word in words:
+            trie_add(word)
 
         output = []
-        visited = [[False] * n for i in range(m)]
-        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        visited = [[False] * n for _ in range(m)]
 
-        def dfs(i, j, root):
-            item = board[i][j]
+        def backtrack(node, i, j):
             if visited[i][j]:
                 return
 
             visited[i][j] = True
-            if '.' in root[item]:
-                word = root[item]['.']
-
-                output.append(word)
-                del root[item]['.']
+            if '.' in node:
+                output.append(node['.'])
+                trie_del(trie, node['.'])
 
             for di, dj in directions:
                 ni, nj = i + di, j + dj
-                if ni >= 0 and ni < m and nj >= 0 and nj < n and board[ni][nj] in root[item]:
-                    dfs(ni, nj, root[item])
+                if ni < 0 or nj < 0 or ni >= m or nj >= n:
+                    continue
+                
+                if board[ni][nj] in node:
+                    backtrack(node[board[ni][nj]], ni, nj)
 
             visited[i][j] = False
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] in trie:
-                    dfs(i, j, trie)
+                    backtrack(trie[board[i][j]], i, j)
 
         return output
