277f0602509a9d4a2bc08aacd64df6344c555811
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-01-01 17:29:45 -0500

    Time: 44 ms (28.42%), Space: 35 MB (51.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,11 +5,42 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        arr = []
-        cur = head
-
+        # Find length
+        length, cur = 0, head
         while cur:
-            arr.append(cur.val)
+            length += 1
             cur = cur.next
+
+        # Find start of second part
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
-        return arr == arr[::-1]
+        start = slow
+        if length % 2 == 1:
+            start = slow.next
+        
+        # Reverse second part
+        prev = None
+        cur = start
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        
+        # Two pointers
+        i = head
+        j = prev
+
+        while i and j:
+            if i.val != j.val:
+                return False
+
+            i = i.next
+            j = j.next
+
+        return True
