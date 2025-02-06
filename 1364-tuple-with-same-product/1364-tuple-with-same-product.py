c250f535aeadf92a7671515fec8cf2d2d6378f62
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-02-05 21:21:13 -0500

    Time: 370 ms (57.35%), Space: 46.3 MB (78.2%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -0,0 +1,18 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        counts = defaultdict(int)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                counts[nums[i] * nums[j]] += 1
+
+        answer = 0
+        for product, count in counts.items():
+            if count <= 1:
+                continue
+            
+            answer += 4 * count * (count - 1)
+
+        return answer
